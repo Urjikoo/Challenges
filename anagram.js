ad9a5b501266ca7521bfb1d9407d4d8be3a5d3c1
urjikoo <urjig122@gmail.com>
2022-02-11 20:57:33 -0500

    leet

--- anagram.js
+++ anagram.js
@@ -2,11 +2,14 @@
 
 function anag(str1, str2) {
   if (str1.length !== str2.length) {
-    return false;
+    //the first thing is making sure if both strings are both the same length. if
+    return false; //not return false!
   }
-  let storage = {};
+  let storage = {}; //declaring an obj to store the letters along with how many times it
+  //appread!
   for (let i = 0; i < str1.length; i++) {
-    let letter = str1[i];
+    //looping though the first string!
+    let letter = str1[i]; //variable to rep teh letter in teh string.
     if (storage[letter]) {
       storage[letter] += 1;
     } else {
