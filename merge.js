b03ae7d223b9d4819d52f15642b0646cd0b28215
urjikoo <urjig122@gmail.com>
2022-02-15 08:24:09 -0500

    Leet

--- merge.js
+++ merge.js
@@ -1,3 +1,37 @@
+// ///if we have [1,2,3,4][3,4,5][1,4,5,6]=>[1,1,2,3,3,4,4,5,5,6]
+
+function mergeIt(lists) {
+  let sortedValues = []; //storing all of the values in sorted manner
+  let listIndex = 0; //how do we compare the values
+  let currentNode = list[listIndex]; //starting from the current index
+  let sortedList;
+  let tail;
+  while (listIndex < lists.length) {
+    //looping through lists
+    if (currentNode.value !== undefined) {
+      //since we don't know the current value of currentNode, if its no undefined
+      sortedValues.push(currentNode.value);
+      currentNode = currentNode.next; //incrementing or pointing to the next node
+      if (!currentNode) {
+        listIndex++;
+        currentNode = lists[listIndex];
+      }
+    }
+  }
+  sortedValues.sort((a, b) => a - b); //since we pushed current node value ,now sorting them
+  sorted.map((el) => {
+    let node = new ListNode(value);
+    if (!sortedList) {
+      sortedList = ListNode(val);
+      tail = sortedList;
+    } else {
+      tail.next = node;
+      tail = tail.next;
+    }
+  });
+  if (!sortedList) return null;
+  return sortedList;
+}
 // You are given the heads of two sorted linked lists list1 and list2.
 
 // Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
